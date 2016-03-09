3d7263042b1e9d4ca62a9661c565eff72e4ffcff
steph-harris <steph.harris21@gmail.com>
2016-03-09 17:06:19 -0500

    added envs package, removed lodash and sequelize

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,3 @@
-require('dotenv').config();
-
 var mysql = require("mysql");
 
 var connection = mysql.createConnection(process.env.JAWSDB_URL);
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+var envs = require('dotenv').config();
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
