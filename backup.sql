244da110af99d8ccec60d00ce5daf04bb6a5b5b9
steph-harris <steph.harris21@gmail.com>
2016-03-09 23:46:44 -0500

    added sql dump

---
+++ backup.sql
@@ -0,0 +1,53 @@
+-- MySQL dump 10.13  Distrib 5.6.27, for osx10.11 (x86_64)
+--
+-- Host: localhost    Database: burgers_db
+-- ------------------------------------------------------
+-- Server version	5.6.27
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `burgers`
+--
+
+DROP TABLE IF EXISTS `burgers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `burgers` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `burger_name` varchar(255) NOT NULL,
+  `devoured` tinyint(1) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `burgers`
+--
+
+LOCK TABLES `burgers` WRITE;
+/*!40000 ALTER TABLE `burgers` DISABLE KEYS */;
+INSERT INTO `burgers` VALUES (1,'CHEDDAR',0,'2016-03-09 21:55:23'),(2,'bacon',1,'2016-02-16 19:37:43'),(3,'Gouda',0,'2016-02-19 05:14:11'),(4,'cheesy',1,'2016-02-19 05:16:08'),(5,'cheesy',0,'2016-02-19 05:14:13'),(6,'mushroom',1,'2016-02-19 05:14:10'),(7,'beef',0,'2016-02-19 05:16:01'),(8,'tofu',1,'2016-02-19 05:16:03'),(11,'kobe',1,'2016-02-19 05:16:07'),(12,'cake',0,'2016-02-19 05:12:35'),(13,'pizza',0,'2016-02-19 05:40:56');
+/*!40000 ALTER TABLE `burgers` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-03-09 23:20:00
